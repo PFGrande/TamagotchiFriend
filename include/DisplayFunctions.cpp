7d7e9dbc5e2dfdde1d1f91f948cf8e0ef0e39c92
PFGrande <2002.pedrogrande@gmail.com>
2023-03-19 21:05:06 -0700

    updated TamagotchiREADME doc

--- include/DisplayFunctions.cpp
+++ include/DisplayFunctions.cpp
@@ -1,9 +1,9 @@
 #include "DisplayFunctions.h"
 
-
+// Prints the character's name on LCD screen
 void printArray(char array[], int startingX, int startingY, int size) {
-  int temp1 = startingX;//Y doesnt change so does not have to be saved
-  //5 is had coded because for some reason the size of method is not working for me
+  int temp1 = startingX; //Y doesnt change so does not have to be saved
+  //5 is had coded because for some reason the sizeOf() is not working for me
   for (int i = 0; i < size; i++){
     lcd_1.setCursor(startingX, startingY);
   	lcd_1.print(array[i]);
@@ -14,14 +14,16 @@ void printArray(char array[], int startingX, int startingY, int size) {
 
 }
 
-void printName (char array[]) {
-  for (int i = 0; i < 5; i++) {
-  	Serial.print(array[i]);
+//Prints the character's name in the console
+// void printName (char array[]) {
+//   for (int i = 0; i < 5; i++) {
+//   	Serial.print(array[i]);
   
-  }
+//   }
 
-}
+// }
 
+//Prints to a row, starting at the start of the defined row 
 void print_line_to_row (String line, int row, bool removeArrow) {
 	lcd_1.setCursor(0, row);
 	lcd_1.print(line);
--- include/DisplayFunctions.h
+++ include/DisplayFunctions.h
@@ -5,10 +5,12 @@
 
 LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2); // Pins used by LCD
 
-void printArray(char array[], int startingX, int startingY, int size);
+// Prints the character's name on LCD screen
+void printArray(char array[], int startingX, int startingY, int size); 
 
-void printName (char array[]);
+//void printName (char array[]);
 
+//Prints to a row, starting at the start of the defined row 
 void print_line_to_row (String line, int row, bool removeArrow);
 
 #endif //PRINT_FUNCTION_H
